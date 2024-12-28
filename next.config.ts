1a4ab765edb2596d7dd4b8d706d3fd63b41985d1
kame_mac <kametyaso@gmail.com>
2024-12-28 23:07:59 +0900

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // MonacoエディターをSSRから除外
+    if (isServer) {
+      config.externals = [...(config.externals || []), "monaco-editor"];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
