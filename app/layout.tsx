7ed7c12b75a25e063a984af137ff50a2efc2366e
kame_mac <kametyaso@gmail.com>
2024-12-28 21:46:46 +0900

    fix: lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
