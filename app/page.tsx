78a3ccc8d0ec4bda113562b2d888b3f7e5c8f517
kame_mac <kametyaso@gmail.com>
2024-12-29 17:31:29 +0900

    fix: view design

--- app/page.tsx
+++ app/page.tsx
@@ -53,17 +53,21 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center md:p-24 p-8">
-      <h2 className="text-3xl mb-4">コードエディター</h2>
-      <div className="mx-auto w-full max-w-xs text-gray-800">
-        <div className="mt-1">
-          <select
-            value={language}
-            onChange={(e) => setLanguage(e.target.value)}
-            className="mt-1 cursor-pointer block w-full rounded-md border border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-          >
-            <option value="ruby">{`Ruby ("3.0.1")`}</option>
-            <option value="javascript">{`JavaScript ("18.15.0")`}</option>
-          </select>
+      <div className="max-w-4xl w-full">
+        <h2 className="text-3xl mb-4">Code Learn App</h2>
+      </div>
+      <div className="max-w-4xl w-full">
+        <div className=" w-full max-w-xs text-gray-800">
+          <div className="mt-1">
+            <select
+              value={language}
+              onChange={(e) => setLanguage(e.target.value)}
+              className="mt-1 cursor-pointer block w-full rounded-md border border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            >
+              <option value="ruby">{`Ruby ("3.0.1")`}</option>
+              <option value="javascript">{`JavaScript ("18.15.0")`}</option>
+            </select>
+          </div>
         </div>
       </div>
       <Editor
@@ -74,21 +78,23 @@ export default function Home() {
         theme="vs-dark"
         onMount={handleEditorDidMount}
       />
-      {loading || (
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-          onClick={showValue}
-        >
-          実行
-        </button>
-      )}
-      {loading && (
-        <div className="flex justify-center" aria-label="読み込み中">
-          <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent mt-4"></div>
-        </div>
-      )}
+      <div className="max-w-4xl w-full">
+        {loading || (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded mt-4 "
+            onClick={showValue}
+          >
+            実行
+          </button>
+        )}
+        {loading && (
+          <div className="flex justify-center" aria-label="読み込み中">
+            <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent mt-4"></div>
+          </div>
+        )}
+      </div>
       <textarea
-        className="max-w-2xl w-full mx-auto md:h-40 h-20 p-2 border text-black mt-8"
+        className="max-w-4xl w-full mx-auto md:h-40 h-20 p-2 border text-black mt-4"
         readOnly={true}
         value={value}
       />
